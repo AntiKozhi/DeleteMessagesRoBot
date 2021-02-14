f17eecd76de0d9e7c0ed87aab2b17a01959f790a
Anti Kozhi Official <79022061+AntiKozhi@users.noreply.github.com>
2021-02-14 23:45:32 +0530

    Update forward.py

--- bot/helpers/forward.py
+++ bot/helpers/forward.py
@@ -9,22 +9,6 @@ async def start_forward(
 ):
     count = 0
     c = 0
-    async for msg in client.iter_history(
-        chat_id=from_channel,
-        limit=None
-    ):
-        if count > 200:
-            await asyncio.sleep(200)
-            count = 0
-        if c == 10000:
-            await asyncio.sleep(10800)
-        if msg.document:
-            message_id = msg.message_id
-            caption = msg.caption
-            
-            await msg.copy(
-                chat_id = to_channel)
-            count = count + 1
-            c = c + 1
+    async for message in client.search_messages(from_channel,filter='document'):
             await asyncio.sleep(3)
-        
+            await message.copy(to_channel)
