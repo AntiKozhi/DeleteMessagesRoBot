30d4ffe1e91b10667c85585e58c3c75326622e67
Anti Kozhi Official <79022061+AntiKozhi@users.noreply.github.com>
2021-02-14 23:13:46 +0530

    Add files via upload

---
+++ bot/helpers/forward.py
@@ -0,0 +1,33 @@
+from typing import List
+import asyncio
+from bot.bot import Bot
+
+async def start_forward(
+    client: Bot,
+    from_channel,
+    to_channel
+):
+    count = 0
+    c = 0
+    async for msg in client.iter_history(
+        chat_id=from_channel,
+        limit=None
+    ):
+        if count > 200:
+            await asyncio.sleep(200)
+            count = 0
+        if c = 10000:
+            await asyncio.sleep(10800)
+        if msg.document:
+            doc_id = msg.document.file_id
+            caption = msg.caption
+            
+            await client.send_document(
+                chat_id = to_channel,
+                document = file_id
+                caption = caption
+            )
+            count = count + 1
+            c = c + 1
+            await asyncio.sleep(3)
+        
